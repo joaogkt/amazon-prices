660beb4f17218fd77b7a45aeb6893d48621f210a
João Torres <116197527+joaogkt@users.noreply.github.com>
2023-10-02 17:56:35 -0300

    Update amazon.py
    Adicionei o link. Para poder acessar o produto se gostar dele

--- amazon.py
+++ amazon.py
@@ -14,8 +14,8 @@ soup = BeautifulSoup(webpage.content, "html.parser")
 nomes = soup.find_all("span", attrs={"class": "a-size-base-plus a-color-base a-text-normal"})
 precos = soup.find_all("span", attrs={"class": "a-price-whole"})
 avaliacoes = soup.find_all("span", attrs={"class": "a-icon-alt"})
-
-min_length = min(len(nomes), len(precos), len(avaliacoes))
+links = soup.find_all("a", attrs={"class": "a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal"})
+min_length = min(len(nomes), len(precos), len(avaliacoes), len(links))
 
 dados = {}
 
@@ -23,12 +23,13 @@ for i in range(min_length):
     nome = nomes[i].text
     preco = precos[i].text
     avaliacao = avaliacoes[i].text
+    link = links[i]['href']
 
     dados[nome] = {
-        'preço': preco,
-        'avaliacao': avaliacao
+        'Preço': preco,
+        'Avaliacao': avaliacao,
+        'Link': "https://amazon.com.br" + link
     }
-
 
 amazon_df = pd.DataFrame.from_dict(dados)
 amazon_df.to_csv("amazon_data.csv", header=True, index=False)
